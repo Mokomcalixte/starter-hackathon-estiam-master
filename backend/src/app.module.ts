81dbf4ab3850a8a54db2a5f88e963e055cb5cce9
wiscod <djihouae@gmail.com>
2026-07-01 00:28:58 +0200

    feat(security): add security features from Membre 2 (AntiFraud, RateLimit) and attack script

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { SecurityModule } from './security/security.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, SecurityModule],
   controllers: [AppController],
   providers: [AppService],
 })
