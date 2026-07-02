c9618b04f4b73288b7ce8b456f15a17d7300fef3
wiscod <djihouae@gmail.com>
2026-07-02 11:05:16 +0200

    feat: Add Docker support, security updates, fix Exec format error, add attack script

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     prefix: '/uploads/',
   })
 
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 
   console.log('✅ Backend démarré')
 }
